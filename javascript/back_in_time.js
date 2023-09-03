d863b7aa3899f986ced0d23ec8d336867e7e1694
Jason Kende <jk@skybound.xyz>
2023-09-03 05:21:04 -0400

    Update back_in_time.js
    Only revert the styles buttons related UI

--- javascript/back_in_time.js
+++ javascript/back_in_time.js
@@ -71,55 +71,55 @@
             })
 
             // Replace Icon with Text
-            const dir_btn = document.getElementById(mode + '2img_open_folder')
-            dir_btn.innerHTML = 'Open Output Folder'
-            dir_btn.classList.remove('tool')
+            // const dir_btn = document.getElementById(mode + '2img_open_folder')
+            // dir_btn.innerHTML = 'Open Output Folder'
+            // dir_btn.classList.remove('tool')
 
-            const save_btn = document.getElementById('save_' + mode + '2img')
-            save_btn.innerHTML = 'Save Image'
-            save_btn.classList.remove('tool')
+            // const save_btn = document.getElementById('save_' + mode + '2img')
+            // save_btn.innerHTML = 'Save Image'
+            // save_btn.classList.remove('tool')
 
-            const zip_btn = document.getElementById('save_zip_' + mode + '2img')
-            zip_btn.innerHTML = 'Save as Zip'
-            zip_btn.classList.remove('tool')
+            // const zip_btn = document.getElementById('save_zip_' + mode + '2img')
+            // zip_btn.innerHTML = 'Save as Zip'
+            // zip_btn.classList.remove('tool')
 
-            const send_ii_btn = document.getElementById(mode + '2img_send_to_img2img')
-            send_ii_btn.innerHTML = 'Send to img2img'
-            send_ii_btn.classList.remove('tool')
+            // const send_ii_btn = document.getElementById(mode + '2img_send_to_img2img')
+            // send_ii_btn.innerHTML = 'Send to img2img'
+            // send_ii_btn.classList.remove('tool')
 
-            const send_in_btn = document.getElementById(mode + '2img_send_to_inpaint')
-            send_in_btn.innerHTML = 'Send to Inpaint'
-            send_in_btn.classList.remove('tool')
+            // const send_in_btn = document.getElementById(mode + '2img_send_to_inpaint')
+            // send_in_btn.innerHTML = 'Send to Inpaint'
+            // send_in_btn.classList.remove('tool')
 
-            const send_ex_btn = document.getElementById(mode + '2img_send_to_extras')
-            send_ex_btn.innerHTML = 'Send to Extra'
-            send_ex_btn.classList.remove('tool')
+            // const send_ex_btn = document.getElementById(mode + '2img_send_to_extras')
+            // send_ex_btn.innerHTML = 'Send to Extra'
+            // send_ex_btn.classList.remove('tool')
 
-            new_form.appendChild(send_ii_btn)
-            new_form.appendChild(send_in_btn)
-            new_form.appendChild(send_ex_btn)
+            // new_form.appendChild(send_ii_btn)
+            // new_form.appendChild(send_in_btn)
+            // new_form.appendChild(send_ex_btn)
 
-            new_row.appendChild(new_form)
+            // new_row.appendChild(new_form)
 
-            row.parentNode.insertBefore(new_row, row)
+            // row.parentNode.insertBefore(new_row, row)
         })
 
         // Extras
-        const dir_btn = document.getElementById('extras_open_folder')
-        dir_btn.innerHTML = 'Open Output Folder'
-        dir_btn.classList.remove('tool')
+        // const dir_btn = document.getElementById('extras_open_folder')
+        // dir_btn.innerHTML = 'Open Output Folder'
+        // dir_btn.classList.remove('tool')
 
-        const send_ii_btn = document.getElementById('extras_send_to_img2img')
-        send_ii_btn.innerHTML = 'Send to img2img'
-        send_ii_btn.classList.remove('tool')
+        // const send_ii_btn = document.getElementById('extras_send_to_img2img')
+        // send_ii_btn.innerHTML = 'Send to img2img'
+        // send_ii_btn.classList.remove('tool')
 
-        const send_in_btn = document.getElementById('extras_send_to_inpaint')
-        send_in_btn.innerHTML = 'Send to Inpaint'
-        send_in_btn.classList.remove('tool')
+        // const send_in_btn = document.getElementById('extras_send_to_inpaint')
+        // send_in_btn.innerHTML = 'Send to Inpaint'
+        // send_in_btn.classList.remove('tool')
 
-        const send_ex_btn = document.getElementById('extras_send_to_extras')
-        send_ex_btn.innerHTML = 'Send to Extra'
-        send_ex_btn.classList.remove('tool')
+        // const send_ex_btn = document.getElementById('extras_send_to_extras')
+        // send_ex_btn.innerHTML = 'Send to Extra'
+        // send_ex_btn.classList.remove('tool')
     }
 }
 
